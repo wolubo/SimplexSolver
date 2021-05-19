59959afee4c3b92bd62e0882da710bc467856ce4
Wolfgang Bongartz <w.bongartz@external.telekom.de>
2021-05-19 16:48:32 +0200

    Switched to Gradle as build system

--- src/parser/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Enthält alle Klassen, die den Parser bilden. Dabei werden alle Klassen mit dem Tool JavaCC generiert. 
- * Das Input-File für JavaCC ist Grammar.jj, in dem die Grammatik des Parser definiert wird.
- * HINWEIS: Alle Files in diesem Paket mit Ausnahme des Files Grammar.jj können bei Bedarf gelöscht 
- * werden. Sie werden von JavaCC neu generiert. 
- */
-package parser;
